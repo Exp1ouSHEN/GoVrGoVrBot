cc69cdc96f8b88d2981bddb77c5765bf88e9b57f
Ruslan <ruslan.dub.19964596@gmail.com>
2026-06-23 15:09:28 +0300

    fix env ADMIN_ID

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 import os
 
+ADMIN_ID = int(os.getenv("ADMIN_ID"))
 BOT_TOKEN = os.getenv("8896694914:AAFhDFS8KPxmGBXiGdEk_wCX9bUmx0BHsAU")
 MONO_TOKEN = os.getenv("utLTpPIIfkPKgGs1U6Y_sbXwcxIhYIA1HLYpvwEc-N5s")
-ADMIN_ID = int(os.getenv("888614652"))
